ea5b9ec598c1dcf0c8bb0d75bf69a3d1e7c7a8ef
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2025-02-12 09:56:10 +0000

    commit seconda lezione a meta

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ sp_oauth = SpotifyOAuth(
 client_id=SPOTIFY_CLIENT_ID,
 client_secret=SPOTIFY_CLIENT_SECRET,
 redirect_uri=SPOTIFY_REDIRECT_URI,
-scope="user-read-private" #permessi x informazioni dell'utente
+scope="user-read-private", #permessi x informazioni dell'utente
+show_dialog=True #forziamo la richiesta di inserire new credenziali
+
 )
 
-@app.route('/')
-def login():
-    auth_url = sp_oauth.get_authorize_url() #login di spotify
-    return redirect(auth_url)
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
 
 @app.route('/callback')
 def callback():
@@ -29,6 +31,9 @@ def callback():
     token_info = sp_oauth.get_access_token(code) #uso il code per un codice di accesso
     session['token_info'] = token_info #salvo il token nella mia sessione x riutilizzarlo
     return redirect(url_for('home'))
+
+
+    
 
 @app.route('/home')
 def home():
